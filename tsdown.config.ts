2d437974a083d870e9d9444a39faea2efad19b22
Specy <specy.dev@gmail.com>
2025-10-21 13:36:44 +0200

    add fasm

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     copy: [
         './src/deps/gnu-as.elf',
         './src/deps/gnu-ld.elf',
-        './src/deps/nasm.elf'
+        './src/deps/nasm.elf',
+        './src/deps/fasm.elf'
     ]
 })
